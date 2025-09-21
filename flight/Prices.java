321541f13209143c052f3241f4731ea8b2f85a9b
JirasaySayasone <s3974058@student.rmit.edu.au>
2025-09-21 18:56:43 +1000

    Prices.java and SeatTypes.java

--- flight/Prices.java
+++ flight/Prices.java
@@ -1,5 +1,16 @@
 package flight;
 
 public class Prices {
+    private float totalPrice;
+
+    public Prices(float totalPrice) {
+        this.totalPrice = totalPrice;
+    }
+    public float getPrice() {
+        return totalPrice;
+    }
+    public static float sumPrice(float locationPrice, float airline, float seatType) {
+        return locationPrice * airline * seatType;
+    }
     
 }
--- flight/SeatTypes.java
+++ flight/SeatTypes.java
@@ -1,5 +1,24 @@
 package flight;
 
 public class SeatTypes {
+    private String seatType;
+    private String seatLocation;
+
+    public SeatTypes(String seatType, String seatLocation) {
+        this.seatType = seatType;
+        this.seatLocation = seatLocation;
+    }
+    public String getSeatType() {
+        return seatType;
+    }
+    public String getSeatLocation() {
+        return seatLocation;
+    }
+    public void setSeatType(String seatType) {
+        this.seatType = seatType;
+    }
+    public void setSeatLocation(String seatLocation) {
+        this.seatLocation = seatLocation;
+    }
     
 }
