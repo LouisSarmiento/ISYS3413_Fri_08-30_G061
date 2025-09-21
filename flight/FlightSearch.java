57f47c43f1d9b59b03d7560c3140923ad819f272
JirasaySayasone <s3974058@student.rmit.edu.au>
2025-09-21 22:46:17 +1000

    final commit

--- flight/FlightSearch.java
+++ flight/FlightSearch.java
@@ -1,7 +1,11 @@
 package flight;
 import java.util.ArrayList;
 import java.util.List;
+import javax.print.attribute.standard.Destination;
 
 public class FlightSearch {
+    private String searchKeyword;
+    private String Destination;
+    private Date departure;
 
 }
