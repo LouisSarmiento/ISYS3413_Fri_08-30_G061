28a1a4ea4997e156dbaff159fc13e22a99543d89
JirasaySayasone <s3974058@student.rmit.edu.au>
2025-09-21 16:11:08 +1000

    flight.java made

---
+++ Flight/flight.java
@@ -0,0 +1,16 @@
+package flight;
+
+import java.util.Date;
+
+public class Flight {
+    String flightID;
+    String airplaneModel;
+    String destination;
+    Date departure;
+    String departFroml;
+    Date flightDuration;
+    int availableSeat;
+    boolean mealplan;
+
+    
+}
